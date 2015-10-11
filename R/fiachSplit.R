7694d6fa96fff04e7e957798bf9c6d6dbdbd540f
tierneytim <fiachmri@gmail.com>
2015-10-11 14:38:37 +0100

    added function for splitting 4D files

---
+++ R/fiachSplit.R
@@ -0,0 +1,14 @@
+fiachSplit<-function(input){
+  outCode<-getDatatype(input = input)
+  data<-readNii(input,fourD=FALSE)
+  t<-length(data)
+  dir<-dirname(input)
+  base<-basename(input)
+  
+  basNoExt<-gsub("([.]nii|[.]img|[.]hdr|[.]nii[.]gz)",replacement = "",x = base)
+  num<-formatC(1:t, width = nchar(as.character(t)), format = "d", flag = "0")
+  outnames<-paste(dir,"/",basNoExt,"_",num,sep = "")
+  for(i in 1:t){
+    RNiftyReg::writeNifti(image = data[[i]],datatype = outCode,file = outnames[i])
+  }
+}
