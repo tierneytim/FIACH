bb95b92cb47a852538760d5487d474a848922f95
tierneytim <fiachmri@gmail.com>
2016-04-03 16:51:08 +0100

    added log transform argument for kmeansMask

--- R/kmeansMask.R
+++ R/kmeansMask.R
@@ -1,7 +1,14 @@
-kmeansMask<-function(x){
+kmeansMask<-function(x,logData=FALSE,retFit=FALSE){
+  if(logData){
+    x[x<=0]<-1
+    x<-log(x)
+    }
   resamp<-quantile(x,probs = seq(0,1,length.out = 1000))
   mod<-kmeans(resamp,2)
   thresh<-resamp[which.max(abs(diff(mod$cluster)))]
   mask<-ifelse(x<=thresh,0,1)
+  if(retFit){mask<-list(mask=mask,fit=mod$betweenss/mod$totss)}
   return(mask)
 }
+
+
