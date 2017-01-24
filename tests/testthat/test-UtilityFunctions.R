38e79326bf8c179160905ebdca09aad411a12a1b
tierneytim <fiachmri@gmail.com>
2017-01-24 22:21:32 +0000

    conceptual grouping of functions for testing purposes

---
+++ tests/testthat/test-UtilityFunctions.R
@@ -0,0 +1,14 @@
+context("Utility Functions")
+
+test_that("Utility Functions gives correct answers", {
+  #arrMat()
+  #bmatArr()
+  #datatpes()
+  #fd()
+  #fiachMerge()
+  #fiachSplit()
+  #getAnalyzeOrigin()
+  #getDatatype()
+  #hotMetal()
+  #zeroNa()
+})
