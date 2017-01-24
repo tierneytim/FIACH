279e2b6dbd93086250db2b86511674b0e40ab6d7
tierneytim <fiachmri@gmail.com>
2017-01-24 17:31:23 +0000

    tried to make code cleaner with better and more extensive comments

--- R/realign.R
+++ R/realign.R
@@ -43,127 +43,220 @@ realign<-function(files,quality=.9,write=TRUE,plotConvergence=FALSE){
   ##### ARG checking #######
   ##########################
   print("Argument Check")
+  
+  ## files check (character and existance)
   if(!is.character(files)){stop("input should be character strings")}
   exists<-file.exists(files)
   if(!all(exists)){stop("At least one of the specified functional files does not exist")}
+  
+  # quality checks (numeric and between 0 and 1)
   if(!is.numeric(quality)){stop("quality should be a real number between 0 and 1")}
-  if(quality==0){stop("quality must be a greater than 0")}
+  if(quality==0){stop("quality must be greater than 0")}
   if(quality>1){stop("quality must be less than or equal to 1")}
+  
+  # dimension checks( equality and 4d files currently not allowed: i hope to fix this if i can access sub-bricks in RNifti )
   dims<-do.call("rbind",lapply(files,function(x){dumpNifti(x)$dim}))
   if(any(dims[,5]>1)){stop("Currently this function does not support  4D files")}
   if(any(dims[1,]!=t(dims))){stop("Not all image dimensions are the same as the target image")}
+  
+  # write check (must be logical)
   if(!is.logical(write)){stop("write must be TRUE or FALSE")}
+  
+  # plot convergence check (must be logical)
   if(!is.logical(plotConvergence)){stop("plotConvergence must be TRUE or FALSE")}
   ##########################
   ### FILE READ ############
   ##########################
   print("Assuming target image is first image")
+  
   targ<-RNiftyReg::readNifti(files[1])
+  
+  # check for analyze format
   anlz<-dumpNifti(files[1])$magic==""
+  
+  #get the pixel dimensions
   pix<-pixdim(targ)[1:3]
+  
+  # if the image is analyze then try and find and origin and set an appropriate qform and signal warning.
   if(anlz){
+    warning("This file appears to be an Analyze file. Orientation information may not be correct.")
     origin<-getAnalyzeOrigin(files[1])
     .qformFix(targ,origin)
     print("Fixing target q/s form")
   }
-  aff0<-buildAffine(scales = pix[1:3]/4,source = drop(targ))                          ## initial affine used to rescale  target 
+  ## affine matrix to downsample image to 4x4x4mm (same as SPM default).
+  aff0<-buildAffine(scales = pix[1:3]/4,source = drop(targ))                          
   print("Resampling target to Lower Resolution")
-  b <- applyTransform(drop(targ),transform = aff0,internal=NA)                                    ## (Ax = b)
+  b <- applyTransform(drop(targ),transform = aff0,internal=NA)                                   
   ##########################
   ###### FILE MANAGEMENT ###
   ##########################
   print("Creating output filenames")
+  
+  # file paths into directories and files
   dir<-dirname(files)
   fname<-basename(files)
+  
+  # prepend filename with r
   outname<-paste(dir,"/r",fname,sep="")
   rpOutName<-paste(dir[1],"rp.txt",sep="/")
   rpPlotOutName<-paste(dir[1],"rp.pdf",sep="/")
+  
+  # find datatype and check whether it is unsigned
   outCode<-getDatatype(input = files)
   unsigned<-(outCode=="char"||outCode=="uint16"||outCode=="uint32"||outCode=="uint64")
   if(unsigned){print("Output will have  negative values set to zero as input has an unsigned datatype")}
+  
   ##########################
   ####### GRADIENTS ########
   ##########################
   print("Constructing Derivatives")
-  A0 <- do.call("cbind",getImageDerivatives(b))                                                                          ## basis set of derivatives
+  
+  #convert the  list of 6 images to a pixel x 6 matrix
+  A0 <- do.call("cbind",getImageDerivatives(b))                                                                          
   ###########################
   ### INTERESTING VOXELS ####
   ###########################
   print("Identifying Interesting Voxels")
-  co <- .inVox(A0,b,quality = quality,plotConvergence = plotConvergence)                                                                        ## interesting voxels
+  
+  # remove voxels that contribute little to the spatial covariance matrix
+  # returns masked coordinates, derivatives and target image 
+  co <- .inVox(A0,b,quality = quality,plotConvergence = plotConvergence)                                                                        
   ###########################
   ###### REG VARIABLES ######
   ###########################
   print("Initialising Variables for Registration")
-  qA <- qr(co[[2]])                                                                        ## decompose the masked basis set for linear least squares using QR
-  ss<-Inf                                                                                   ## initialize sum of squares
-  maxit<-64                                                                                ## max iterations
-  reg<-b
-  rp<-matrix(0,nrow=sum(dims[,5]),ncol = 6)                                                ## declare matrix to hold realignment paramters
-  aff<-buildAffine(source = b)                                                             ## declare affine to be used in registration
-  bsub <- co[[3]]                                                                          ## masked target as a vector
-  sbsub<-sum(bsub)                                                                         ## sum of masked target as a vector(prevent recalculation in main loop)
+  
+  ## decompose the masked derivatives for linear least squares using QR decomposition
+  ## this is internally the default method for lm and lm.fit which compromises speed for accuracy.
+  qA <- qr(co[[2]])                                                                        
+  
+  ## initialize sum of squares
+  ss<-Inf                                                                                   
+  
+  ## max iterations: set to SPM default
+  maxit<-64
+  
+  ## declare matrix to hold realignment paramters
+  rp<-matrix(0,nrow=sum(dims[,5]),ncol = 6)
+  
+  ## masked target image  as a vector
+  bsub <- co[[3]]                                                                          
+  
+  ## sum of masked target as a vector used to scale images in case of gross global intensity difference
+  sbsub<-sum(bsub)
   ############################
   ##### REGISTRATION #########
   ############################
   print("Registering and Reslicing")
 
+  # initialise a progress bar
   pb <- txtProgressBar(style = 3)
-  for (j in 1:length(files)) {                                                               ## main loop over images
+
+  # loop over j files
+  for (j in 1:length(files)) {                                                               
     
+    # if its the first image 
     if(j==1){
+     # the target image is left unchanged
       reg<-targ
+      # and written to disk
       if(write){writeNifti(image = reg,file = outname[j],datatype = outCode)}
+      # the realignmet parameter matrix initial row is set to 0 because no registration occured
       rp[j,]<-0
+      # update the progress bar
       setTxtProgressBar(pb, j/length(files))
       next
     }
-   
+    
+    # read in the jth source  image 
     sourceF<-readNii(input = files[j])
+    
+    # if analyze image is detected
     if(anlz){
+    # set the qform to something hopefully more appropriate
+    # a warning should have triggred earlier so I won't repeat
       .qformFix(sourceF,origin = origin)
     }
-    aff0<-buildAffine(scales = pix[1:3]/4,source = drop(sourceF))                          ## initial affine used to rescale  target 
+    
+    ## initial affine transform used to rescale  source 
+    aff0<-buildAffine(scales = pix[1:3]/4,source = drop(sourceF))                          
     rfunc<-applyTransform(transform = aff0,interpolation = 1,x = sourceF,internal=NA)
+    
+    # identity affine that will be recursively updated sending source image into target space
     aff<-buildAffine(source = rfunc)
     
-    for (i in 1:maxit) {                                                                    ## registration loop
-      Fl <- applyTransform(transform = aff,x = rfunc,interpolation = 1,internal=NA)[co[[1]]]              ## apply potentially updated transform                                                       ## the masked target as a vector
-      sc <- sbsub/sum(Fl)                                                                     ## ratio of the source and the target 
-      b1 <- bsub - Fl * sc                                                                    ## scale source so mean intensity ~= target
-      pss<-ss;                                                                                ## declare the previous sum of squares
-      ss<-sum(b1*b1)                                                                          ## calculate new sum of squares
-      if(i==1){conv<-Inf}else{conv<-(pss-ss)/pss}                                             ## define the convergence criteria
+    ## registration loop
+    for (i in 1:maxit) {
+      # sample the image at the coordinates held in co[[1]] having applied the potentiall updated aff Transform
+      Fl <- applyTransform(transform = aff,x = rfunc,interpolation = 1,internal=NA)[co[[1]]]
+      
+      ## ratio of the source and the target to be used as a scale factor to account for gross global differences in intensity
+      sc <- sbsub/sum(Fl)                                                                     
+      
+      ##  difference between tareget and  scaled source image(~= mean intensity due to scaling)
+      b1 <- bsub - Fl * sc                                                                    
+      
+      ## declare the previous sum of squares
+      pss<-ss 
+      
+      ## calculate new sum of squares
+      ss<-sum(b1*b1)                                                                          
+      
+      ## define the convergence criteria
+      if(i==1){conv<-Inf}else{conv<-(pss-ss)/pss}
       if(pss == 0){break()}
-      if ( conv< 1e-8 || i == maxit){break()}                                                 ## finish if convergerd or max iterations are reached
-      soln<- qr.coef(qr = qA,y = b1)                                                          ## use base qr to get coef 
-      tmp<-buildAffine(translation = soln[1:3],angles = soln[4:6],source = rfunc)             ## build affine with updates
-      aff<-composeTransforms(transform1 = tmp,aff)                                            ## compose updates with previous solution
-    }                                                                                       ## end registraion loop          
     
-    hmm<-decomposeAffine(aff)                                                                 ## decompose the rigid transformation
-    rp[j,]<-c(hmm$translation,hmm$angles)                                                     ## get translations and rotations
-    finAff<-buildAffine(translation = rp[j,1:3],angles = rp[j,4:6],source = sourceF)          ## build the final affine
-    reg<-applyTransform(transform = finAff,x = sourceF,internal = NA)                                       ## apply final affine to input images
+      ## finish if convergerd or max iterations are reached
+      if ( conv< 1e-8 || i == maxit){break()}                                                 
+      
+      ## the realignment parameters are obtained by solving Ax=b(qAx=b1)
+      soln<- qr.coef(qr = qA,y = b1)                                                          
+      
+      ## construct temporary affine that contains the solution
+      tmp<-buildAffine(translation = soln[1:3],angles = soln[4:6],source = rfunc)             
+      
+      # update final affine with the temporary one by composing them
+      aff<-composeTransforms(transform1 = tmp,aff)                                            
+    }                                                                                       
+    ## decompose the rigid transformation and place in the realignmet parameter matrix
+    hmm<-decomposeAffine(aff)                                                                 
+    rp[j,]<-c(hmm$translation,hmm$angles)                                                     
     
+    ## build the final affine and apply the transform
+    finAff<-buildAffine(translation = rp[j,1:3],angles = rp[j,4:6],source = sourceF)          
+    reg<-applyTransform(transform = finAff,x = sourceF,internal = NA)                 
+    
+    ## set negative values to 0 if unsigned data is being used(really should change this behaviour)
     if(unsigned){
      reg[reg<0]<-0
       }
-   
+    
+    ## If writing is requested then write the resliced images to a file
     if(write){writeNifti(image = reg,file = outname[j],datatype = outCode)}
+    
+    # update  the progress bar
     setTxtProgressBar(pb, j/length(files))
   } 
+  # close the progress bar
   close(pb)
   #############################
   ######## PARAMETRS ##########
   #############################
+  # if writing is requested
   if(write){
+    
+    ## create a pdf of the realignemnt paramters
     pdf(file = rpPlotOutName,width = 8.27,height = 11.69)
     par(mfrow=c(2,1))
     ts.plot(rp[,1:3,drop=FALSE],main="Translation Paramteres",xlab="Time (scans)",ylab="Distance (mm)")
     ts.plot(rp[,4:6,drop=FALSE]*57,main="Rotation Parameters",xlab="Time (scans)",ylab="Rotation (Degrees)")
     dev.off()
+    
+    # and write the paramters to a .txt file.
     write.table(rp,file = rpOutName,row.names = FALSE,col.names = FALSE)
   }
+  
+  # return the parameters
   return(rp)
 }
