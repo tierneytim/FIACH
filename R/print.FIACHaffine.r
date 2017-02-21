f54c76c3744de09322095e80c99bb798bf79b4cd
tierneytim <fiachmri@gmail.com>
2017-02-21 00:14:31 +0000

    update print Function and added documentation

--- R/print.FIACHaffine.r
+++ R/print.FIACHaffine.r
@@ -1,4 +1,8 @@
 print.FIACHAffine <- function(x, ...) {
+  affineClass <- class(x)
+  if (affineClass != "FIACHAffine") {
+    stop(paste("class: ", affineClass, " is not supported.\n objects of class FIACHAffine are supported "))
+}  
   cat("FIACH affine matrix:\n")
   lines <- apply(format(x,scientific=FALSE), 1, paste, collapse="  ")
   cat(paste(lines, collapse="\n"))
