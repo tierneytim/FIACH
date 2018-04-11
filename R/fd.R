555b317d59300dd4c0c251bd17b66c55a8d7a2bd
Laurent Schüpbach <contact+github@laurentschuepbach.ch>
2018-04-11 15:19:05 +0200

    Corrected fd calculation

--- R/fd.R
+++ R/fd.R
@@ -6,12 +6,10 @@ fd<-function(input){
   disp<-rp[,1:3]
   rad<-rp[,4:6]
   angDisp<-sin(rad)*50
-  
+
   totDisp<-cbind(disp,angDisp)
   dif<-apply(totDisp,2,diff)
-  dif.sq<-dif^2
-  fd<-sqrt(rowSums(dif.sq))
+  fd<-rowSums(abs(dif))
 
   return(c(0,fd))
 }
-
